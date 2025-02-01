4ece5d8d267a3213740b2a115fb3fdbd0a404421
bromscandium <kkmshbiu@protonmail.com>
2025-02-01 17:30:57 +0100

    header file for game

--- game.h
+++ game.h
@@ -8,6 +8,7 @@
 // Function declarations
 void game(WINDOW *win, int settings[]);
 void movement(const char ch, int playerpos[]);
+void results(WINDOW *win, int *settings, int golds, int destroys, int playedtime, int life, int time, int scores);
 void second();
 
 #endif // GAME_H
